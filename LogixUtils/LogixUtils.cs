9a9ad8f844039d42f068890f4b5bd000337051b5
badhaloninja <badhaloninja@badhalo.ninja>
2022-01-09 17:26:52 -0600

    Temp fix for Label fighting with other users Hopefully temporary, I don't want to resort to localizing the label

--- LogixUtils/LogixUtils.cs
+++ LogixUtils/LogixUtils.cs
@@ -16,7 +16,7 @@ namespace LogixUtils
     {
         public override string Name => "LogixUtils";
         public override string Author => "badhaloninja";
-        public override string Version => "1.3.1";
+        public override string Version => "1.3.2";
         public override string Link => "https://github.com/badhaloninja/LogixUtils";
         public override void OnEngineInit()
         {
@@ -115,6 +115,53 @@ namespace LogixUtils
                 }
                 return StringHelper.BeautifyName(type.Name);
             }
+
+
+            [HarmonyPatch(typeof(LogixTip))]
+            class MultiplayerFix  // Fix Label fighting when in session with users who don't have the mod
+            {
+                /* Apparently Last Modifying user gets cleared immediately???
+                 * 
+                [HarmonyReversePatch]
+                [HarmonyPatch("UpdateSpawnLabel")]
+                public static void UpdateSpawnLabel(LogixTip instance)
+                {
+                    // its a stub so it has no initial content
+                    throw new NotImplementedException("It's a stub");
+                }
+                [HarmonyPostfix]
+                [HarmonyPatch("Update")]
+                public static void Update(LogixTip __instance, SyncRef<TextRenderer> ___Label) // Update label of own tool if it was last updated by someone else
+                {
+                    if (___Label.Target == null) return;
+                    Sync<string> LabelTextField = ___Label.Target.Text;
+                    if (LabelTextField.WasLastModifiedBy(__instance.LocalUser) || LabelTextField.LastModifyingUser == null) return; //Return if last modifying user was not current user
+                    UpdateSpawnLabel(__instance);
+                }
+                [HarmonyPrefix] 
+                [HarmonyPatch("OnChanges")]
+                public static bool OnChanges(LogixTip __instance, SyncType ___ActiveNodeType) // Prevent triggering on other users' LogixTips
+                {
+                    Msg(___ActiveNodeType.LastModifyingUser);
+                    return (__instance.Slot.ActiveUser == __instance.LocalUser || // Continue if activeUser is local user OR
+                        __instance.Slot.ActiveUser == null && ___ActiveNodeType.WasLastModifiedBy(__instance.LocalUser)); // if ActiveUser is null AND node type was last modified by LocalUser
+                }*/
+
+
+
+                // ValueCopy version
+                // This works fine, but I don't want to resort to making it local
+                [HarmonyPrefix]
+                [HarmonyPatch("OnChanges")]
+                public static void OnChanges(LogixTip __instance, SyncRef<TextRenderer> ___Label)
+                {
+                    if ((__instance.Slot.ActiveUser != null && __instance.Slot.ActiveUser != __instance.LocalUser) || ___Label.Target == null) return; //Return if ActiveUser is not local user unless null   Also Return if Label is null
+
+                    Sync<string> LabelTextField = ___Label.Target.Text;
+                    if (LabelTextField.IsDriven || LabelTextField.IsHooked) return;
+                    LabelTextField.DriveFrom(LabelTextField, writeBack: true).Persistent = false;
+                }
+            }
         }
         
         
@@ -130,5 +177,42 @@ namespace LogixUtils
                 }
             }
         }
+
+
+
+
+
+/*        [HarmonyPatch(typeof(LogixTip))]
+        class debugFunny
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch("OnAttach")]
+            public static void OnAttach(LogixTip __instance)
+            {
+                string name = __instance.Slot.ActiveUser.UserName;
+                Msg("OnAttach Triggered, ActiveUser: " + name + ", RefID: " + __instance.ReferenceID);
+            }
+            [HarmonyPrefix]
+            [HarmonyPatch("OnChanges")]
+            public static void OnChanges(LogixTip __instance)
+            {
+                string name = __instance.Slot.ActiveUser.UserName;
+                Msg("OnChanges Triggered, ActiveUser: " + name + ", RefID: " + __instance.ReferenceID);
+            }
+            [HarmonyPrefix]
+            [HarmonyPatch("SetActiveType")]
+            public static void SetActiveType(LogixTip __instance)
+            {
+                string name = __instance.Slot.ActiveUser.UserName;
+                Msg("SetActiveType Triggered, ActiveUser: " + name + ", RefID: " + __instance.ReferenceID);
+            }
+*//*            [HarmonyPrefix]
+            [HarmonyPatch("Update")]
+            public static void Update(LogixTip __instance)
+            {
+                string name = __instance.Slot.ActiveUser.UserName;
+                Msg("Update Triggered, ActiveUser: " + name + ", RefID: " + __instance.ReferenceID);
+            }*//*
+        }*/
     }
 }
